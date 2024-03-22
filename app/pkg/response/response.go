40a1fb89e1285a9eee8169e74cab31a3a359ce24
zwn <1879154660@qq.com>
2024-03-22 17:58:31 +0800

    feat: 修改成功请求返回的消息

--- app/pkg/response/response.go
+++ app/pkg/response/response.go
@@ -1,50 +1,50 @@
 package response
 
 import (
-    "github.com/gin-gonic/gin"
-    cErr "github.com/jassue/gin-wire/app/pkg/error"
-    "net/http"
+	"github.com/gin-gonic/gin"
+	cErr "github.com/jassue/gin-wire/app/pkg/error"
+	"net/http"
 )
 
 type Response struct {
-    ErrorCode int `json:"error_code"`
-    Data interface{} `json:"data"`
-    Message string `json:"message"`
+	ErrorCode int         `json:"code"`
+	Data      interface{} `json:"data"`
+	Message   string      `json:"message"`
 }
 
 func ServerError(c *gin.Context, err interface{}) {
-    msg := "Internal Server Error"
-    if gin.Mode() != gin.ReleaseMode {
-        if _, ok := err.(error); ok {
-            msg = err.(error).Error()
-        }
-    }
-    FailByErr(c, cErr.InternalServer(msg))
+	msg := "Internal Server Error"
+	if gin.Mode() != gin.ReleaseMode {
+		if _, ok := err.(error); ok {
+			msg = err.(error).Error()
+		}
+	}
+	FailByErr(c, cErr.InternalServer(msg))
 }
 
 func Success(c *gin.Context, data interface{}) {
-    c.JSON(http.StatusOK, Response{
-        0,
-        data,
-        "ok",
-    })
-    c.Abort()
+	c.JSON(http.StatusOK, Response{
+		20000,
+		data,
+		"ok",
+	})
+	c.Abort()
 }
 
 func Fail(c *gin.Context, httpCode int, errorCode int, msg string) {
-    c.JSON(httpCode, Response{
-        errorCode,
-        nil,
-        msg,
-    })
-    c.Abort()
+	c.JSON(httpCode, Response{
+		errorCode,
+		nil,
+		msg,
+	})
+	c.Abort()
 }
 
 func FailByErr(c *gin.Context, err error) {
-    v, ok := err.(*cErr.Error)
-    if ok {
-        Fail(c, v.HttpCode(), v.ErrorCode(), v.Error())
-    } else {
-        Fail(c, http.StatusBadRequest, cErr.DEFAULT_ERROR, err.Error())
-    }
+	v, ok := err.(*cErr.Error)
+	if ok {
+		Fail(c, v.HttpCode(), v.ErrorCode(), v.Error())
+	} else {
+		Fail(c, http.StatusBadRequest, cErr.DEFAULT_ERROR, err.Error())
+	}
 }
